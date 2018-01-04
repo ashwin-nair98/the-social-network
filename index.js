375b2b61cca0243e0364d54410a5e12ce8d15a51
Ashwin B Nair <ashiwn.bala.432@gmail.com>
2018-01-04 15:00:56 +0530

    Prototype using template engines, views setup

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8080);
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/signin', function(req, res){
+	res.render('signin');
+});
+
+app.get('/signup', function(req, res){
+	res.render('signup');
+});
+
+app.listen(app.get('port'), function(){
+	console.log("Server listening to port:", app.get('port'));
+});
