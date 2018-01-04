ccbea4661d3507074941307435fe08b8ce809e03
Ashwin B Nair <ashwin.bala.432@gmail.com>
2018-01-04 15:19:54 +0530

    Modified Partial views

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
 	res.render('index');
 });
